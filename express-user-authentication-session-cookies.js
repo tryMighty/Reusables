aa6d86fdb7b4077988cdcdbdb9cc6fe3618d1ebe
Cipher <teminimighty@gmail.com>
2026-03-31 19:28:05 +0100

    Refactor to use environment variables for config
    Updated session secret and database connection details to use environment variables.

--- express-user-authentication-session-cookies.js
+++ express-user-authentication-session-cookies.js
@@ -5,16 +5,19 @@ import bcrypt from "bcrypt";
 import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import env from "dotenv";
+
 
 const app = express();
 const port = 3000;
 const saltRounds = 10;
+env.config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use(session({
-  secret: "TOPSECRET, // use .env
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -27,11 +30,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "database",
-  password: "password",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
